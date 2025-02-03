bbfc1d93b3acc5e166591f2973eb13d7384d3638
Your Name <you@example.com>
2025-02-03 23:48:22 +0530

    changes of responsiveness

--- assets/navbar.js
+++ assets/navbar.js
@@ -64,3 +64,56 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+
+// toggle action close buttton
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.querySelector(".hamburger");
+    const navContainer = document.querySelector(".nav-container");
+
+    hamburger.addEventListener("click", () => {
+        navContainer.classList.toggle("active"); // Show/hide menu
+        hamburger.classList.toggle("active"); // Change to "X"
+    });
+
+    // Close menu when clicking outside
+    document.addEventListener("click", (event) => {
+        if (!navContainer.contains(event.target) && !hamburger.contains(event.target)) {
+            navContainer.classList.remove("active");
+            hamburger.classList.remove("active");
+        }
+    });
+});
+
+
+// pop-up card
+
+// pop-up card
+document.addEventListener("DOMContentLoaded", function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const flagValue = urlParams.get('emailSuccess');
+    const popupMessage = document.getElementById("popup-message");
+    const popupHeading = document.getElementById("popup-heading");
+    const popupPara = document.getElementById("popup-para");
+    const closePopup = document.getElementById("close-popup");
+
+    if (flagValue === 'true') {
+        popupHeading.innerHTML = "EMAIL SENT SUCCESSFULLY";
+        popupPara.innerHTML = "Thanks for Your Subscription"; // Clear any existing message
+        popupMessage.style.display = "block";
+    } else if (flagValue === 'false') {
+        popupHeading.innerHTML = "EMAIL NOT SENT";
+        popupPara.innerHTML = "There was an error sending your message. Please try again later.";
+        popupMessage.style.display = "block";
+    }
+
+    closePopup.addEventListener("click", function () {
+        popupMessage.style.display = "none";
+    });
+
+    // Get the current URL without the query parameters
+    const baseUrl = window.location.href.split('?')[0];
+
+    // Replace the current state with a new state without the query parameters
+    history.replaceState(null, null, baseUrl);
+});
